fcca926ae53ddb1786b6b51807ddfe7f0cc296ab
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-05-02 01:43:07 +0400

    Everything is documented

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """function"""
     tasks = []
     start_time = time.time()
     for i in range(4):
