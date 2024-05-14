cb393474f006f307e4b1156d5fcef9ba99e75a0d
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-05-14 19:37:05 +0400

    added eslint-disable

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-// Import the ClassRoom class from the specified file
 /* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/extensions
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
     return [
         new ClassRoom(19),
         new ClassRoom(20),
-        new ClassRoom(34)
+        new ClassRoom(34),
     ];
 }
