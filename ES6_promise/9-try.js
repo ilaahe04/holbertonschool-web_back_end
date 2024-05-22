c21ec240f12913d5502a6cac15bcf2166fba6a52
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-05-22 17:24:32 +0400

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+export default function guardrail(mathFunction) {
+	const arr = [];
+
+	try {
+		arr.push(mathFunction());
+	} catch (err) {
+		arr.push(err.toString());
+	} finally {
+		arr.push('Guardrail was processed');
+	}
+	return arr;
+}
