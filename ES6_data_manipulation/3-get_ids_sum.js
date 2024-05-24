7cabb2cae563e55494e032f0cc5ff37fbf67e6de
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-05-24 06:57:36 +0400

    reduce

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+/*eslint*disable*/
+export default function getStudentIdsSum(students) {
+	if (!Array.isArray(students)) {
+		return 0;
+	}
+
+	return students.reduce((sum, student) => sum + student.id, 0)
+}
